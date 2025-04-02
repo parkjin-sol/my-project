8da82832d89b74a3cd3e3b89c511eb1341bec3d1
parkjins-sol <soljinpark1223@gmail.com>
2025-04-02 23:15:21 +0900

    20250402_마이프로젝트

--- src/include/Footer.jsx
+++ src/include/Footer.jsx
@@ -1,7 +1,11 @@
 const Footer = () => {
     return (
         <>
-            
+            <footer>
+                <div className="foot-area">
+                    &copy; Made by Park Jinsol 2025. All rights reserved.
+                </div>
+            </footer>
         </>
     )
 };
